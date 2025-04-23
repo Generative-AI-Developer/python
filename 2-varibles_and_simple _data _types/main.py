4a81969cbfd12cd98291724fa239b693b5169377
Muhammad Asif <programmerasif@outlook.com>
2025-04-23 12:48:02 +0500

    strings

--- 2-varibles_and_simple _data _types/main.py
+++ 2-varibles_and_simple _data _types/main.py
@@ -3,3 +3,15 @@ print("Hello world")
 message = "This is a test message to print a variable"
 
 print(message)
+
+name = "Muhammad Asif"
+print(name.upper())
+
+first_name = "Muhammad"
+seconed_name = "Asif"
+full_name = f"{first_name} {seconed_name}"
+print(full_name)
+print(full_name.lower())
+print(full_name.title())
+print(full_name.capitalize())
+print(full_name.strip())
