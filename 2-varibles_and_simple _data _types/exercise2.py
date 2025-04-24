6b8999e4a08bd85f996fd5351878625e71024774
Muhammad Asif <programmerasif@outlook.com>
2025-04-24 11:44:41 +0500

    exercises added

---
+++ 2-varibles_and_simple _data _types/exercise2.py
@@ -0,0 +1,20 @@
+'''2-8. File Extensions: Python has a removesuffix() method that works exactly
+like removeprefix(). Assign the value 'python_notes.txt' to a variable called
+filename. Then use the removesuffix() method to display the filename without
+the file extension, like some file browsers do.
+'''
+text = "hello.txt"
+print(text)
+result = text.removesuffix(".txt")
+print(result)
+
+text = "hello.txt"
+print(text)
+result = text.removeprefix("he")
+print(result)
+
+
+text = "hello.txt"
+print(text)
+result = text.removesuffix("xt")
+print(result)
