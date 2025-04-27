71d8a70f7ab60979a8d5398a96dbe779a1466686
Muhammad Asif <programmerasif@outlook.com>
2025-04-27 14:30:25 +0500

    exercise added

---
+++ lists/exercise1.py
@@ -0,0 +1,10 @@
+names = ["Asif", "Ali", "Awais", "Hassan", "Ali"]
+
+message = f"Hello, {names[0]} ! Welcome to the Python programming world."
+print(message)
+
+message = f"Hello, {names[1]} ! Welcome to the Python programming world."
+print(message)
+
+message = f"Hello, {names[2]} ! Welcome to the Python programming world."
+print(message)
